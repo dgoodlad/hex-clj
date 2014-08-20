2d26dafc18f04608928b6646e273e347f6b265ee
David Goodlad <david@goodlad.net>
2014-08-19 21:27:39 -0700

    Add neighbor calculations

--- src/hex_clj/core.clj
+++ src/hex_clj/core.clj
@@ -1,1 +1,22 @@
-(ns hex-clj.core)
+(ns hex-clj.core
+  (:require [hex-clj.coords :as c]))
+
+(def cube-neighbors
+  [[ 1 -1 0] [ 1 0 -1] [0  1 -1]
+   [-1  1 0] [-1 0  1] [0 -1  1]])
+
+(def axial-neighbors
+  [[ 1 0] [ 1 -1] [0 -1]
+   [-1 0] [-1  1] [0  1]])
+
+(defn cube-neighbor
+  "Returns the neighboring hex using cube coordinates"
+  [dir [x y z :as hex]]
+  (let [neighbor (cube-neighbors dir)]
+    (map + neighbor hex)))
+
+(defn axial-neighbor
+  "Returns the neighboring hex using axial coordinates"
+  [dir [q r :as hex]]
+  (let [neighbor (axial-neighbors dir)]
+    (map + neighbor hex)))
--- test/hex_clj/core_test.clj
+++ test/hex_clj/core_test.clj
@@ -1,3 +1,12 @@
 (ns hex-clj.core-test
   (:require [clojure.test :refer :all]
             [hex-clj.core :refer :all]))
+
+(deftest test-neighbors
+  (testing "Neighbors in cube coordinates"
+    (let [hex [2 3 -7]]
+      (is (= [3 2 -7] (cube-neighbor 0 hex)))
+      (is (= [3 3 -8] (cube-neighbor 1 hex)))))
+  (testing "Neighbors in axial coordinates"
+    (let [hex [2 -7]]
+      (is (= [3 -7] (axial-neighbor 0 hex))))))
