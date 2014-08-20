77deeca166620b542eca1a543c5e55bdbfafd770
David Goodlad <david@goodlad.net>
2014-08-19 21:56:49 -0700

    Add distance calculations

--- src/hex_clj/core.clj
+++ src/hex_clj/core.clj
@@ -1,5 +1,6 @@
 (ns hex-clj.core
-  (:require [hex-clj.coords :as c]))
+  (:require [hex-clj.coords :as c]
+            [clojure.math.numeric-tower :as math]))
 
 (def cube-neighbors
   [[ 1 -1 0] [ 1 0 -1] [0  1 -1]
@@ -20,3 +21,12 @@
   [dir [q r :as hex]]
   (let [neighbor (axial-neighbors dir)]
     (map + neighbor hex)))
+
+(defn distance
+  [[x1 y1 z1 :as a] [x2 y2 z2 :as b]]
+  (/ (reduce + (map math/abs (map - a b))) 2))
+
+(defn axial-distance
+  [a b]
+  (distance (apply c/axial-to-cube a)
+            (apply c/axial-to-cube b)))
--- test/hex_clj/core_test.clj
+++ test/hex_clj/core_test.clj
@@ -10,3 +10,15 @@
   (testing "Neighbors in axial coordinates"
     (let [hex [2 -7]]
       (is (= [3 -7] (axial-neighbor 0 hex))))))
+
+(deftest test-distance
+  (testing "Distance from the origin"
+    (let [origin [0 0 0]]
+      (is (= 1 (distance origin (cube-neighbor 0 origin))))
+      (is (= 2 (distance origin [2 0 -2])))
+      (is (= 2 (distance origin [-1 2 -1])))
+      (is (= 4 (distance origin [-2 4 -2])))))
+  (testing "Axial coordinate distance"
+    (let [origin [0 0]]
+      (is (= 1 (axial-distance origin (cube-neighbor 0 origin))))
+      (is (= 2 (distance origin [2 -2]))))))
